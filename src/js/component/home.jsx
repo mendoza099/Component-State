d2e9289b684b53ea06d31e754c672f0bfc11aba4
mendoza099 <sergiomendozadominguez@gmail.com>
2022-05-30 19:10:35 +0000

    componen state

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-
+import "./styles.css";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color1, setColor1] = useState("red");
+
+	function changeColor() {
+		color1 === "red"
+			? setColor1("este boton debe estar encendido ")
+			: setColor1("este boton debe estar apagado");
+	}
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<ul className="traffic-light">
+				{lights.map((isActive, index) => (
+					<li key={index} onClick={() => setActiveLight(isActive, index)}>
+						<span className={isActive ? "on" : "off"} />
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
